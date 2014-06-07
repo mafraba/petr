e859e4de2b2eb779b55c1a37c439b20052a5ab16
Manuel Franco <mafraba@gmail.com>
2014-06-08 00:53:03 +0200

    Tools for organisations now described at the home page
    (refs #1)

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -15,5 +15,5 @@ Then(/^I should see what the system offers to individuals$/) do
 end
 
 Then(/^I should see what the system offers to organizations$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_selector('main article #tools_for_organisations')
 end
