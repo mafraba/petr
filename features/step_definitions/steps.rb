95281f2e8f79830d832a8118e8d9d38c5fb5d4d8
Manuel Franco <mafraba@gmail.com>
2014-06-07 22:42:00 +0200

    Project's purpose now stated at home page
    (refs #1)

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,7 +7,7 @@ Then(/^I should see a welcome message$/) do
 end
 
 Then(/^I should see a description of the system's main purposes$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_selector('main article #purpose')
 end
 
 Then(/^I should see what the system offers to individuals$/) do
