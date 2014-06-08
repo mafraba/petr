3d19066934cfb892cadb72a57a0a7e57b0875e87
Manuel Franco <mafraba@gmail.com>
2014-06-08 19:31:11 +0200

    Password forgotten scenario
    (refs #2)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  config.action_dispatch.show_exceptions = true
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
@@ -31,9 +31,14 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Default url (devise needs it)
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -13,7 +13,11 @@ Given(/^I am logged in$/) do
   click_button "Sign in"
 end
 
-
+Given(/^I am a registered user$/) do
+  email = 'testing@man.net'
+  password = 'secretpass'
+  Organisation.new(:email => email, :password => password, :password_confirmation => password).save!
+end
 
 When(/^I click the 'Sign Up' link$/) do
   click_link 'signup-link'
@@ -36,7 +40,30 @@ Then(/^I should have registered myself as an organisation$/) do
   expect(Organisation).to exist(email: 'testing@man.net')
 end
 
+When(/^I click the 'Login' link$/) do
+  click_link 'login-link'
+end
 
+When(/^I click the 'Password forgotten' link$/) do
+  click_link 'password-forgotten-link'
+end
+
+When(/^I fill in my email$/) do
+  email = 'testing@man.net'
+  fill_in "organisation_email", :with => email
+end
+
+When(/^I push the 'Send me reset password instructions' button$/) do
+  click_button 'Send me reset password instructions'
+end
+
+
+Then(/^I should be sent an email with instructions$/) do	  
+  last_email = ActionMailer::Base.deliveries.last
+  expect(last_email.to).to eq ['testing@man.net']  
+  expect(last_email.subject).to have_content 'Reset password instructions'
+  expect(last_email.body).to have_content 'link to change your password'
+end
 
 
 Then(/^I should see a 'Sign up' link$/) do
