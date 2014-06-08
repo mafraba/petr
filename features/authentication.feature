3e07e7f2c435c0fff10809afa9aca25ce55e3c52
Manuel Franco <mafraba@gmail.com>
2014-06-08 14:27:33 +0200

    Authentication feature: User logged in and user sign up scenarios
    (refs #2)

--- features/authentication.feature
+++ features/authentication.feature
@@ -1,10 +1,24 @@
 Feature: Authentication of users
   	In order to be authenticated as a registered organisations
   	The users
-  	Should be able to signup, log in and out, reset password
+  	Should be able to signup, log in and out, reset password, and also allowed to edit
+  	their data once they are logged in.
 
-Scenario: The user is not loged in and visits the home page
+Scenario: The user is not logged in and visits the home page
 	Given I am not logged in
   	When I visit the home page
   	Then I should see a 'Sign up' link
   	And I should see a 'Login' link
+
+Scenario: The user is logged in and visits the home page
+	Given I am logged in
+  	When I visit the home page
+  	Then I should see a 'Edit profile' link
+  	And I should see a 'Logout' link
+
+Scenario: The user should be able to sign up
+	Given I am not logged in
+  	When I click the 'Sign Up' link
+  	And I fill my data
+  	And I submit the signup form
+  	Then I should have registered myself as an organisation
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -2,10 +2,55 @@ Given(/^I am not logged in$/) do
   page.driver.submit :delete, "/users/sign_out", {}
 end
 
+Given(/^I am logged in$/) do
+  email = 'testing@man.net'
+  password = 'secretpass'
+  User.new(:email => email, :password => password, :password_confirmation => password).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Sign in"
+end
+
+
+
+When(/^I click the 'Sign Up' link$/) do
+  click_link 'signup-link'
+end
+
+When(/^I fill my data$/) do
+  email = 'testing@man.net'
+  password = 'secretpass'
+
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  fill_in "user_password_confirmation", :with => password
+end
+
+When(/^I submit the signup form$/) do
+  click_button "Sign up"
+end
+
+Then(/^I should have registered myself as an organisation$/) do
+  expect(User).to exist(email: 'testing@man.net')
+end
+
+
+
+
 Then(/^I should see a 'Sign up' link$/) do
   expect(page).to have_selector('#signup-link')
 end
 
 Then(/^I should see a 'Login' link$/) do
   expect(page).to have_selector('#login-link')
+end
+
+Then(/^I should see a 'Edit profile' link$/) do
+  expect(page).to have_selector('#edit-profile-link')
+end
+
+Then(/^I should see a 'Logout' link$/) do
+  expect(page).to have_selector('#logout-link')
 end
