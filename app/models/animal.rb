51b6b544713d43749641ad8f88ea23194f053299
Manuel Franco <mafraba@gmail.com>
2014-06-09 22:55:31 +0200

    Animal model rspec
    (refs #4)

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,15 @@
 class Animal < ActiveRecord::Base
   belongs_to :organisation
+
+  # Should always have a name
+  validates_presence_of :name
+  # Should always have a sex
+  validates_presence_of :sex
+  # Should always have a birth date
+  validates_presence_of :birthdate
+  # Should always have a description
+  validates_presence_of :description
+
+  # Sex should be either 'male' or 'female'
+  validates :sex, inclusion: { in: %w(male female) }
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Animal do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # It should always belong to an organisation
+  it { should belong_to(:organisation) }
+  
+  it "should always have a name" do
+  	should validate_presence_of(:name)
+  end
+
+  it "should always have a sex" do
+  	should validate_presence_of(:sex)
+  end
+
+  it "should either be male or female" do
+  	should ensure_inclusion_of(:sex).in_array(%w( male female ))
+  end
+
+  it "should always have a birth date" do
+  	should validate_presence_of(:birthdate)
+  end
+
+  it "should always have a birth description" do
+  	should validate_presence_of(:description)
+  end
+
 end
