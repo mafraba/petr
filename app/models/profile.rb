3a89d6a39033074aa1a13c7ce861bc47c933bf86
Manuel Franco <manuel-david.franco@c-globalservices.es>
2014-07-03 14:04:41 +0200

    Overwriting original uploads with resized ones
    (refs #23)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,7 +8,14 @@ class Profile < ActiveRecord::Base
     s3_credentials: "#{Rails.root}/config/aws.yml",
     path: ":class/:attachment/:id/:style/:filename",
     url: ':s3_domain_url',
-    styles: { :thumbnail => '100×100' }
+    styles: { 
+      :original => '1920×1080>',
+      :thumbnail => '100×100>' 
+    },
+    convert_options: { 
+      :original => "-strip",
+      :thumbnail => "-quality 75 -strip -background transparent -gravity center -extent 100x100"
+    }
 
   # Validate content type
   validates_attachment_content_type :logo, :content_type => /\Aimage/
