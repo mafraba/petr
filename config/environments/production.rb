c865d14ecfb8deedbc17eabb62e45d5f0669e8c2
Manuel Franco <mafraba@gmail.com>
2014-07-08 17:31:12 +0200

    Mailer SMPT config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'petr-dev.heroku.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address    => "smtp.gmail.com",
+    :port       => 587,
+    :user_name  => ENV['MAILER_USERNAME'],
+    :password   => ENV['MAILER_PASSWORD'],
+    :domain     => 'whatever',
+    :authentication  => :plain
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
