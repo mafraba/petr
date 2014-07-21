3d8e346aeb1d9d2b91f771f259a57b93e5de2273
Manuel David Franco Barrios <mafraba@gmail.com>
2014-07-21 12:48:41 +0200

    Update s3_direct_upload.rb

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,6 +1,6 @@
 S3DirectUpload.config do |c|
-  c.access_key_id = "AKIAIU7UWD3DBK4T4DJA"
-  c.secret_access_key = "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"
+  c.access_key_id = ""
+  c.secret_access_key = ""
   c.bucket = "eu-petrescue-dev"
   c.region = nil             # region prefix of your bucket url. This is _required_ for the non-default AWS region, eg. "s3-eu-west-1"
   c.url = "http://eu-petrescue-dev.s3.amazonaws.com/"
